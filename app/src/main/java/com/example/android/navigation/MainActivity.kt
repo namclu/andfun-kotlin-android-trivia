673ef21d7043c4f6e99760e0e4afff4e1076e896
Nam Lu <namlu1205@gmail.com>
2019-06-10 11:19:22 +1000

    Add TitleFragment

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -25,7 +25,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val binding =
+                DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 
     // TODO (01) Create the new TitleFragment
